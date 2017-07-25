0348a8c9868789bb6970e21d0139b40824246d55
andrewjc88 <andrewjc88@gmail.com>
2017-07-25 12:53:46 -0700

    Adds cats array and cat selector list.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,69 @@
+var initialCats = [
+    {
+        name: "Tabby",
+        clickCount: 0,
+        imgSrc: "img/22252709_010df3379e_z.jpg",
+        imgAttribution: 'https://www.flicker.com/photos/bigcats/',
+        nickNames: ['buddy', 'Snuggly', 'Fuzzy']
+    },
+    {
+        name: "Frank",
+        clickCount: 0,
+        imgSrc: "img/434164568_fea0ad4013_z.jpg",
+        imgAttribution: 'https://www.flicker.com/photos/bigcats/',
+        nickNames: ['buddy', 'Snuggly', 'Fuzzy']
+    },
+    {
+        name: "George",
+        clickCount: 0,
+        imgSrc: "img/1413379559_412a540d29_z.jpg",
+        imgAttribution: 'https://www.flicker.com/photos/bigcats/',
+        nickNames: ['buddy', 'Snuggly', 'Fuzzy']
+    },
+    {
+        name: "Little",
+        clickCount: 0,
+        imgSrc: "img/4154543904_6e2428c421_z.jpg",
+        imgAttribution: 'https://www.flicker.com/photos/bigcats/',
+        nickNames: ['buddy', 'Snuggly', 'Fuzzy']
+    },
+    {
+        name: "Skittle",
+        clickCount: 0,
+        imgSrc: "img/9648464288_2516b35537_z.jpg",
+        imgAttribution: 'https://www.flicker.com/photos/bigcats/',
+        nickNames: ['buddy', 'Snuggly', 'Fuzzy']
+    }
+];
+
+
 function ViewModel() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flicker.com/photos/bigcats/');
-    this.nickNames = ko.observable (['buddy', 'Snuggly', 'Fuzzy']);
+    var self = this;
+
+    this.changeCat = function(clickedCat) {
+        self.currentCat(clickedCat);
+    };
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    })
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+};
+
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nickNames = ko.observable (data.nickNames);
 
     this.title = ko.computed(function() {
         var title;
@@ -23,7 +79,7 @@ function ViewModel() {
         }
         return title;
     }, this);
-
 };
+
 
 ko.applyBindings(new ViewModel());
